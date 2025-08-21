10f89cf4c725dc4f775143f26843e6082f44bbaf
Brent Seibert <brent.seibert@closeknit.com>
2025-08-21 13:31:49 -0400

    Fix incorrect SOURCES reference

--- jdbc/.scripts/mcpservernodriver.java
+++ jdbc/.scripts/mcpservernodriver.java
@@ -1,6 +1,6 @@
 //intermediatesmall shim to make possible to run the server without the jar
 //being published to a repository.
-//SOURCES ../src/main/java/io/quarkus/mcp/MCPServerJDBC.java
+//SOURCES ../src/main/java/io/quarkus/mcp/servers/jdbc/MCPServerJDBC.java
 //FILES ../src/main/resources/application.properties
 //DEPS io.quarkus.platform:quarkus-bom:3.18.1@pom
 //DEPS io.quarkiverse.mcp:quarkus-mcp-server-stdio:1.0.0.Beta1
