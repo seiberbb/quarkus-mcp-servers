fb92d5e2de3ad2a2a9d42e55e4b188171446de60
Brent Seibert <brent.seibert@closeknit.com>
2025-09-04 12:29:00 -0400

    Update dependency versions for on-demand building jdbcnodriver version

--- jdbc/.scripts/mcpservernodriver.java
+++ jdbc/.scripts/mcpservernodriver.java
@@ -2,6 +2,6 @@
 //being published to a repository.
 //SOURCES ../src/main/java/io/quarkiverse/mcp/servers/jdbc/MCPServerJDBC.java
 //FILES ../src/main/resources/application.properties
-//DEPS io.quarkus.platform:quarkus-bom:3.18.1@pom
-//DEPS io.quarkiverse.mcp:quarkus-mcp-server-stdio:1.0.0.Beta1
+//DEPS io.quarkus.platform:quarkus-bom:3.20.1@pom
+//DEPS io.quarkiverse.mcp:quarkus-mcp-server-stdio:1.2.0
 
